9ceabfee18f8a5e46740e1fcac8b8d61982af99f
ArwenAlcaraz <142982183+ArwenAlcaraz@users.noreply.github.com>
2024-06-22 22:39:48 +0800

    Update GlobalStyles.js

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,14 +1,15 @@
 /* fonts */
 export const FontFamily = {
   poppinsSemiBold: "Poppins-SemiBold",
-  poppinsBold: "Poppins-Bold",
+  poppinsExtraBold: "Poppins-ExtraBold",
 };
 /* font sizes */
 export const FontSize = {
   size_2xl: 21,
-  size_sm: 14,
   size_lgi: 19,
   size_lg: 18,
+  size_sm: 14,
+  size_mini: 15,
   size_smi: 13,
 };
 /* Colors */
@@ -20,9 +21,13 @@ export const Color = {
   colorBlack: "#000",
   colorAntiquewhite: "#f9e2d0",
   colorSnow: "#fffcfc",
+  colorSnow_100: "#fffbfb",
+  colorSnow_200: "#fffafa",
+  colorDarkslategray: "#132a17",
 };
 /* border radiuses */
 export const Border = {
   br_xl: 20,
   br_4xs: 9,
+  br_mini: 15,
 };
